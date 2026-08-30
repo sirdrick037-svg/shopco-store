f00438a5cc4ec5adac4399d76b0d5feca3468ab9
sirdrick037 <sirdrick 037@gmail.com>
2026-08-30 13:38:56 +0300

    edit main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import App from "./App";
+import App from "./App.jsx";
 
-import { AuthProvider } from "./context/AuthContext";
-import { CartContextProvider } from "./context/AuthContext";
+import { AuthProvider } from "./context/AuthContext.jsx";
+import { CartProvider } from "./context/CartContext.jsx";
 
 import "./index.css";
 
 ReactDOM.createRoot(
   document.getElementById("root")
-
 ).render(
   <React.StrictMode>
+
     <AuthProvider>
-      <CartContextProvider>
+
+      <CartProvider>
+
         <App />
-      </CartContextProvider>
+
+      </CartProvider>
+
     </AuthProvider>
+
   </React.StrictMode>
 );
